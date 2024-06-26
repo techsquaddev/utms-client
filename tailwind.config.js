45b3f3d6588101395a952390820cd5077747338a
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-06-26 12:02:57 +0530

    create a simple theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1E3A8A",
+        secondary: "#10B981",
+        accent: "#F59E0B",
+      },
+      screens: {
+        sm: "576px",
+        md: "800px",
+        lg: "1440px",
+      },
+    },
   },
   plugins: [],
 };
