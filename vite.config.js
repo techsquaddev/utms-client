f65a7935de2523252963bc25885bf5e63c02cd72
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-07-24 21:56:16 +0530

    feat: add server URL

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://utms-server.onrender.com",
         changeOrigin: true,
       },
     },
