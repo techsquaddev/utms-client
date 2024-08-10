943b04c790b36a57f35ae0d0e70f83513ec8063a
Hasitha Peiris <hasithapeiris.me@gmail.com>
2024-08-11 00:14:17 +0530

    feat: complete contact and about pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,25 +9,44 @@ const About = () => {
           <h1 className="text-center text-xl text-text font-bold">
             About SLIIT 360
           </h1>
-          <div className="mt-7 mb-4 p-5 mx-5 bg-white border-2 border-secondary rounded-3xl shadow-lg">
+          <div className="mt-7 mb-4 p-5 mx-5 bg-white border-2 border-secondary rounded-3xl shadow-md">
             <p className="text-soft-text text-left text-sm md:text-base">
               <span className="font-semibold bg-secondary">
-                SLIIT 360 is an online material hub for SLIIT students.
+                Welcome to SLIIT 360, an online material hub for all students at
+                SLIIT!
               </span>{" "}
-              In particular, we focus on providing user-friendly timetables
-              instead of hard-to-read HTML schedules. In the near future, we
-              plan to add important links and notices as well.
+              <br />
             </p>
             <br />
             <p className="text-soft-text text-left text-sm md:text-base">
-              Currently, we are adding timetables to the system, so some
-              timetables may not be available in the system. If you can't find
-              your timetable, send us an email and we'll add it as soon as
-              possible. But in the near future, we hope to provide features to
-              add timetables yourself. So stay with us.
+              In this platform, we made it our goal to give everyone access to
+              user-friendly timetables as opposed to hard-to-read HTML
+              schedules. In the near future, we plan to add important links and
+              notices here as well.
             </p>
           </div>
-          <div className="p-5 mx-auto mt-4 md:p-10">
+
+          <div className="mb-4 p-5 mx-5 bg-white border-2 border-secondary rounded-3xl shadow-md">
+            <p className="text-soft-text text-left text-sm md:text-base">
+              Currently, we are still adding timetables to the system, so you
+              may find some timetables unavailable.
+            </p>
+            <br />
+            <p className="text-soft-text text-left text-sm md:text-base">
+              In case anyone finds that their timetables are missing in the
+              system, send us an email and we'll add it as soon as possible.
+            </p>
+          </div>
+
+          <div className="p-5 mx-5 bg-white border-2 border-secondary rounded-3xl shadow-md">
+            <p className="text-soft-text text-left text-sm md:text-base">
+              We have plans for a feature that allows you to add your own
+              timetables in the works, so stay tuned with SLIIT 360 and spread
+              the word!
+            </p>
+          </div>
+
+          <div className="p-5 mx-auto  md:p-10">
             <img src={about} alt="about" className="w-full object-cover" />
           </div>
         </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,33 +1,37 @@
 import { Wrapper } from "@/components";
 import { contact } from "@/assets";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 const Contact = () => {
   return (
     <div>
       <Wrapper>
-        <div className="mt-10">
+        <div className="mt-10 flex flex-col items-center">
           <h1 className="text-center text-xl text-text font-bold">
             Contact Us
           </h1>
-          <div className="mt-7 mb-4 p-5 mx-5 bg-white border-2 border-secondary rounded-3xl shadow-lg">
+          <div className="mt-7 p-5 mx-5 bg-white border-2 border-secondary rounded-3xl shadow-lg">
             <p className="text-soft-text text-left text-sm md:text-base">
-              <span className="font-semibold bg-secondary">
-                SLIIT 360 is an online material hub for SLIIT students.
-              </span>{" "}
-              In particular, we focus on providing user-friendly timetables
-              instead of hard-to-read HTML schedules. In the near future, we
-              plan to add important links and notices as well.
-            </p>
-            <br />
-            <p className="text-soft-text text-left text-sm md:text-base">
-              Currently, we are adding timetables to the system, so some
-              timetables may not be available in the system. If you can't find
-              your timetable, send us an email and we'll add it as soon as
-              possible. But in the near future, we hope to provide features to
-              add timetables yourself. So stay with us.
+              If you can't find your timetable on SLIIT 360, don't worry! Just
+              drop us an email, and we'll make sure to add your timetable as
+              soon as possible. We encourage batch representatives to inform us
+              about timetable changes.
             </p>
           </div>
-          <div className="p-5 mx-auto md:p-10">
+          <div
+            className="flex items-center justify-center gap-2 mt-5 p-2 mx-5 bg-white border-2 border-primary rounded-xl shadow-lg text-text"
+            style={{ width: "fit-content" }}
+          >
+            <MailOutlineIcon
+              fontSize="string"
+              className="text-xl md:text-2xl"
+            />
+
+            <h2 className="text-center text-base font-medium md:text-lg">
+              sliit360.me@gmail.com
+            </h2>
+          </div>
+          <div className="p-5 mt-2 mx-auto md:p-10">
             <img src={contact} alt="contact" className="w-full object-cover" />
           </div>
         </div>
