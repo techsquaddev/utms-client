fd818d2b73adf01b5fdc02a3dc9621a165365c21
Hasitha <hasithapeiris.me@gmail.com>
2025-01-19 21:06:49 +0530

    fix: delete unnecessary files

--- src/admin/Timetables.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Timetables = () => {
-  return <div>Timetables</div>;
-};
-
-export default Timetables;
--- src/admin/index.js
+++
@@ -1,6 +0,0 @@
-import Dashboard from "./Dashboard";
-import Home from "./Home";
-import Timetables from "./Timetables";
-import Users from "./Users";
-
-export { Dashboard, Home, Timetables, Users };
