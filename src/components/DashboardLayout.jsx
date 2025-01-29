74eada81718d67978495049602f0a39b2243daed
Hasitha <hasithapeiris.me@gmail.com>
2025-01-29 10:28:54 +0530

    fix: update dashboard layout

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
 import Logo from "../assets/sliit360.svg";
 import { Link, Outlet, useLocation } from "react-router-dom";
-import { Footer } from ".";
 import { useAuth } from "@/api/authContext";
 
 const DashboardLayout = () => {
@@ -18,11 +16,12 @@ const DashboardLayout = () => {
   ];
 
   return (
-    <div className="h-screen p-4 bg-[#f4f7fb]">
-      <div className="grid grid-cols-12 gap-4 xl:h-[91%]">
-        <div className="col-span-2 bg-gray-100 rounded-lg shadow-md">
-          <div className="p-4 mb-10">
-            <Link to="/">
+    <div className="flex flex-col h-screen">
+      <div className="flex-1 grid grid-cols-12 gap-4 overflow-hidden p-4">
+        {/* Sidebar */}
+        <div className="col-span-2 bg-gray-100/80 backdrop-blur-lg border rounded-lg flex flex-col justify-between p-4">
+          <div>
+            <Link to="/" className="flex justify-start">
               <div className="p-3 bg-primary rounded-xl shadow-lg max-w-fit">
                 <img
                   className="w-16 object-contain md:w-20"
@@ -31,36 +30,43 @@ const DashboardLayout = () => {
                 />
               </div>
             </Link>
+
+            {/* Navigation Links */}
+            <div className="mt-8 space-y-2">
+              {sideNavigation
+                .filter((item) => item.key !== "users" || user.role === "admin")
+                .map((item) => (
+                  <Link
+                    key={item.key}
+                    to={item.path}
+                    className={`group flex items-center border p-3 cursor-pointer rounded-lg transition duration-200 ${
+                      location.pathname === item.path
+                        ? "bg-gray-200 font-medium"
+                        : "hover:bg-gray-200"
+                    }`}
+                  >
+                    <span>{item.label}</span>
+                  </Link>
+                ))}
+            </div>
           </div>
-          <div className="w-full h-fit">
-            {sideNavigation
-              .filter((item) => item.key !== "users" || user.role === "admin")
-              .map((item) => (
-                <Link
-                  key={item.key}
-                  to={item.path}
-                  className={`flex border py-4 px-8 items-center cursor-pointer hover:bg-gray-300 transition duration-200 ${
-                    location.pathname === item.path ? "bg-gray-300" : ""
-                  } rounded-lg`}
-                >
-                  <span>{item.label}</span>
-                </Link>
-              ))}
-            {user && (
-              <button
-                onClick={logout}
-                className="py-4 px-8 border justify-start flex cursor-pointer hover:bg-gray-300 transition duration-200 rounded-lg w-full"
-              >
-                Logout
-              </button>
-            )}
-          </div>
+
+          {/* Logout Button */}
+          {user && (
+            <button
+              onClick={logout}
+              className="py-3 px-6 border flex cursor-pointer hover:bg-gray-200 transition duration-200 rounded-lg w-full text-left"
+            >
+              Logout
+            </button>
+          )}
         </div>
-        <div className="col-span-10 p-4 ml-4 rounded-xl bg-gray-100 shadow-md max-h-full overflow-hidden">
+
+        {/* Main Content */}
+        <div className="lg:col-span-10 md:col-span-9 col-span-12 p-4 rounded-xl bg-gray-100/80 border backdrop-blur-lg max-h-full overflow-auto">
           <Outlet />
         </div>
       </div>
-      <Footer />
     </div>
   );
 };
