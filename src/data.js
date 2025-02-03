9092e283e1d5824b48838b49aa811237c1f71934
Hasitha <hasithapeiris.me@gmail.com>
2025-02-03 14:31:15 +0530

    fix: fix home image width issue

--- src/data.js
+++ src/data.js
@@ -2,28 +2,4 @@ export const faculties = ["FOC", "FOB", "FOE", "FOHAS", "SOA", "FOGSAR"];
 
 export const specializations = {
   FOC: ["IT", "SE", "DS", "CSNE", "CS", "ISE", "IM", "CSE"],
-  Other: ["Other"],
-};
-
-export const facultyNames = [
-  "Select Faculty",
-  "Faculty of Computing",
-  "Faculty of Business",
-  "Faculty of Engineering",
-  "Other",
-];
-
-export const departments = {
-  "Faculty of Computing": [
-    "Information Technology",
-    "Software Engineering",
-    "Data Science",
-    "CSNE",
-    "CS",
-    "ISE",
-    "IM",
-    "CSE",
-    "Other",
-  ],
-  Other: ["Other"],
 };
